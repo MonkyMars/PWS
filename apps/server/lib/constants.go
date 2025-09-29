977cfc79c0db8e3e39a6fe496fb56cb0065264d3
Levi Noppers <levinoppers@proton.me>
2025-09-29 23:15:52 +0200

    Formatting and ci fix

--- apps/server/lib/constants.go
+++ apps/server/lib/constants.go
@@ -7,37 +7,32 @@ const (
 	RefreshTokenCookieName = "refresh_token"
 )
 
-var (
-	ErrInvalidCredentials   = errors.New("invalid credentials")
-	ErrUserNotFound         = errors.New("user not found")
-	ErrFailedToRefreshToken = errors.New("failed to refresh token")
-	ErrFailedToDeleteToken  = errors.New("failed to delete token")
-)
-
 const (
 	RoleAdmin   = "admin"
 	RoleTeacher = "teacher"
 	RoleStudent = "student"
 )
 
-const (
-	ErrInvalidCredentials = errors.New("invalid credentials")
-	ErrUserNotFound       = errors.New("user not found")
-	ErrUserAlreadyExists  = errors.New("user already exists")
-	ErrUsernameTaken      = errors.New("username already taken")
-	ErrHashingPassword    = errors.New("error hashing password")
-	ErrCreateUser         = errors.New("error creating user")
-	ErrInvalidToken       = errors.New("invalid token")
-	ErrExpiredToken       = errors.New("expired token")
-	ErrGeneratingToken    = errors.New("error generating token")
-	ErrValidatingToken    = errors.New("error validating token")
+var (
+	ErrInvalidCredentials   = errors.New("invalid credentials")
+	ErrUserNotFound         = errors.New("user not found")
+	ErrUserAlreadyExists    = errors.New("user already exists")
+	ErrUsernameTaken        = errors.New("username already taken")
+	ErrHashingPassword      = errors.New("error hashing password")
+	ErrCreateUser           = errors.New("error creating user")
+	ErrInvalidToken         = errors.New("invalid token")
+	ErrExpiredToken         = errors.New("expired token")
+	ErrGeneratingToken      = errors.New("error generating token")
+	ErrValidatingToken      = errors.New("error validating token")
+	ErrFailedToRefreshToken = errors.New("failed to refresh token")
+	ErrFailedToDeleteToken  = errors.New("failed to delete token")
 )
 
 const (
-  TableUsers           = "users"
+	TableUsers           = "users"
 	TableFiles           = "files"
 	TableSubjects        = "subjects"
 	TableUserOAuthTokens = "user_oauth_tokens"
 	TableUserSubjects    = "user_subjects"
-  TableAuditLogs       = "audit_logs"
+	TableAuditLogs       = "audit_logs"
 )
