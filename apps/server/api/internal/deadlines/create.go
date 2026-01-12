9255362dfaf6c4fba6d4e68c74d76de6f050d197
supperclip <supperclip@gmail.com>
2026-01-12 17:35:08 +0000

    third draft

--- apps/server/api/internal/deadlines/create.go
+++ apps/server/api/internal/deadlines/create.go
@@ -2,18 +2,21 @@ package deadlines
 
 import (
 	"github.com/MonkyMars/PWS/api/middleware"
+	"github.com/MonkyMars/PWS/api/response"
 	"github.com/MonkyMars/PWS/config"
 	"github.com/MonkyMars/PWS/lib"
+	"github.com/MonkyMars/PWS/services"
 	"github.com/gofiber/fiber/v3"
 	"github.com/google/uuid"
 )
 
 type CreateDealineRequest struct {
-	SubjectID uuid.UUID `json:"subject_id"`
-	OwnerID uuid.UUID `json:"owner_id"`
-	Title string `json:"title"`
-	Description string `json:"description"`
-	DueDate string `json:"due_date"`
+	SubjectID   uuid.UUID `json:"subject_id"`
+	OwnerID     uuid.UUID `json:"owner_id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	DueDate     string    `json:"due_date"`
+	CreatedAt   string    `json:"created_at"`
 }
 
 func CreateDeadline(c fiber.Ctx) error {
@@ -24,7 +27,10 @@ func CreateDeadline(c fiber.Ctx) error {
 		logger.Error("Failed to get validated request", "error", err)
 		return lib.HandleValidationError(c, err, "request")
 	}
-	
-	
 
+	if body == nil {
+		return response.NotFound(c, "Data not found")
+	}
+
+	return response.Accepted(c, "Deadline creation accepted")
 }
