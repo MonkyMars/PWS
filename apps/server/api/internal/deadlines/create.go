ea5e302fccc8ee5e423484b21c00f2da010a745e
supperclip <supperclip@gmail.com>
2026-01-12 12:09:23 +0000

    first draft

---
+++ apps/server/api/internal/deadlines/create.go
@@ -0,0 +1,30 @@
+package deadlines
+
+import (
+	"github.com/MonkyMars/PWS/api/middleware"
+	"github.com/MonkyMars/PWS/config"
+	"github.com/MonkyMars/PWS/lib"
+	"github.com/gofiber/fiber/v3"
+	"github.com/google/uuid"
+)
+
+type CreateDealineRequest struct {
+	SubjectID uuid.UUID `json:"subject_id"`
+	OwnerID uuid.UUID `json:"owner_id"`
+	Title string `json:"title"`
+	Description string `json:"description"`
+	DueDate string `json:"due_date"`
+}
+
+func CreateDeadline(c fiber.Ctx) error {
+	logger := config.SetupLogger()
+
+	body, err := middleware.GetValidatedRequest[CreateDealineRequest](c)
+	if err != nil {
+		logger.Error("Failed to get validated request", "error", err)
+		return lib.HandleValidationError(c, err, "request")
+	}
+	
+	
+
+}
